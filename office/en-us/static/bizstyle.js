3f2593480e6db13d23853803ccba78da3c6e8a94
solos <lxl1217@gmail.com>
2026-03-31 17:38:38 +0800

    update docs
    update docs

--- office/en-us/static/bizstyle.js
+++ office/en-us/static/bizstyle.js
@@ -17,7 +17,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Hedwi work suite  documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "BindOffice  documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
